1a01bf834458b6c489e8c88382d046d4ba90a6e3
Lucas Exequiel Pérez Maidana <lucasexe91@hotmail.com>
2021-04-10 10:21:58 -0300

    version de pruebas iluaye

---
+++ src/routes/index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const router= express.Router();
+const pool = require('../database');
+
+
+router.get('/', async (req, res) => {
+    try {
+        const links = await pool.query('SELECT * FROM emprendedor');
+        res.render('links/inicio', {links});
+      } catch (error) {
+        console.error(error);
+      }
+    
+    res.render('links/inicio');
+});
+router.get('/inicio', async (req, res)=>{
+    try {
+        const links = await pool.query('SELECT * FROM emprendedor');
+        res.render('links/inicio', {links});
+      } catch (error) {
+        console.error(error);
+      }
+});
+router.get('/tienda', async (req, res)=>{
+    try{
+        const links = await pool.query('SELECT *  FROM producto');
+        res.render('links/tienda', {links});
+    }catch(error){
+        console.error(error);
+    }
+});
+router.get('/quienesomos', (req, res)=>{
+    res.render('links/quienesomos');
+});
+    
+    router.get('/emprendedor/:idemprendedor', async (req, res)=>{
+        try{
+            const { idemprendedor } = req.params;
+            const esteemprendedor = await pool.query('SELECT * FROM emprendedor WHERE idemprendedor = ?', [idemprendedor]);
+            res.render('links/emprendedor',{esteemprendedor});
+        }catch(e){
+            console.log(e);
+        }
+    });
+
+
+
+
+module.exports = router;
